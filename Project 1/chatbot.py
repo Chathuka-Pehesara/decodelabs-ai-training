fe994589d8d5a224c2ca8f0c51eb7038e5728fd8
Chathuka-Pehesara <ChathukaPehesara02@gmail.com>
2026-06-21 12:37:11 +0530

    Implement the terminal  banner

--- Project 1/chatbot.py
+++ Project 1/chatbot.py
@@ -8,3 +8,16 @@ COLOR_THEME: Dict[str,str] = {
     "USER": "\033[97m",
     "RESET": "\033[0m"
 }
+
+def print_banner() -> None:
+    border = "=" * 60
+    title = "CPBOT (v1.0.0)"
+    tagline = "A portfolio-Grade Rule-based AI Chatbot for DecodeLabs"
+
+    print(f"{COLOR_THEME['SYSTEM']}{border}")
+    print(f"{title:^60}")
+    print(f"{tagline:^60}")
+    print(f"{border}{COLOR_THEME['RESET']}\n")
+    print(f"{COLOR_THEME['SYSTEM']}System: Type 'help' to see what I can do or 'exit' to quit.{COLOR_THEME['RESET']}\n")
+    
+print_banner()
